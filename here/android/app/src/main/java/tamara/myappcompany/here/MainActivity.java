0f73806f9276879b26d0429f24acde62e7690f5e
TK <tamarakruljevic@gmail.com>
2020-09-08 08:52:59 -0400

    Initial commit

---
+++ here/android/app/src/main/java/tamara/myappcompany/here/MainActivity.java
@@ -0,0 +1,49 @@
+package tamara.myappcompany.here;
+
+import android.content.Intent;
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentTransaction;
+
+import org.tensorflow.lite.examples.detection.R;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        if (savedInstanceState == null) {
+            getSupportFragmentManager()
+                    .beginTransaction()
+                    .add(R.id.container, new LoginFragment())
+                    .commit();
+        }
+    }
+
+    /**
+     * Navigate to the given fragment.
+     *
+     * @param fragment       Fragment to navigate to.
+     * @param addToBackstack Whether or not the current fragment should be added to the backstack.
+     */
+
+    public void navigateTo(Fragment fragment, boolean addToBackstack) {
+        FragmentTransaction transaction =
+                getSupportFragmentManager()
+                        .beginTransaction()
+                        .replace(R.id.container, fragment);
+
+        if (addToBackstack) {
+            transaction.addToBackStack(null);
+        }
+
+        transaction.commit();
+    }
+
+
+
+}
---
+++ here/android/app/src/main/java/tamara/myappcompany/here/NavigationHost2.java
@@ -0,0 +1,15 @@
+package tamara.myappcompany.here;
+
+import androidx.fragment.app.Fragment;
+
+/**
+ * A host (typically an {@code Activity}} that can display fragments and knows how to respond to
+ * navigation events.
+ */
+public interface NavigationHost2 {
+    /**
+     * Trigger a navigation to the specified fragment, optionally adding a transaction to the back
+     * stack to make this navigation reversible.
+     */
+    void navigateTo(DetectorActivity fragment, boolean addToBackstack);
+}
